fdd4b9c0056138554e65d15b27aab6fee654e13a
siddharth23052006 <65166961+siddharth23052006@users.noreply.github.com>
2020-06-19 13:05:51 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,27 @@
+class Box{
+    constructor(x,y,width,height,color){
+        var options = {
+            'restitution':0.5,
+            'density':0.7,
+            'friction':0.5,
+        }
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.color = color;
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);    
+    }
+    display(){
+        var angle = this.body.angle;
+        var pos = this.body.position;
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        strokeWeight(2);
+        stroke(0);
+        fill(this.color);
+        rect(0,0,this.width,this.height);
+        pop();
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Project-C29
+Project-C29
